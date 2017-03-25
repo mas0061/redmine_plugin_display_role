49a096e3feb674ee708bb76ee3ce26e94c47bad8
mas0061 <mas0061@users.noreply.github.com>
2017-03-26 07:45:19 +0900

    add settings of this plugin

--- assets/javascripts/redmine_plugin_display_role.js
+++ assets/javascripts/redmine_plugin_display_role.js
@@ -1,8 +1,8 @@
-function insert_role_on_top(role) {
+function insert_role_on_top(role, color) {
   var current_project_name = $('h1 span.current-project').html();
   console.log(current_project_name);
 
   if (current_project_name && $('#loggedas').length) {
-    $('#loggedas').prepend('<span style="color: red;">' + role + '</span> ');
+    $('#loggedas').prepend('<span style="color: ' + color + ';">' + role + '</span> ');
   }
 }
--- init.rb
+++ init.rb
@@ -8,4 +8,8 @@ Redmine::Plugin.register :redmine_plugin_display_role do
   version '0.0.1'
   url 'https://github.com/mas0061/redmine_plugin_display_role'
   author_url 'https://github.com/mas0061'
+
+  settings :default => {
+    :role_text_color => 'red'
+  }, :partial => 'redmine_plugin_display_role/display_role_settings'
 end
