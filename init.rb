1cbaad8b944860bcae46cb3d9df46363225cfc10
mas0061 <mas0061@users.noreply.github.com>
2017-04-09 20:52:08 +0900

    version 0.0.1 -> 1.0.0

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_plugin_display_role do
   name 'Display Role plugin'
   author 'mas0061'
   description 'This plugin display current Role on Redmine.'
-  version '0.0.1'
+  version '1.0.0'
   url 'https://github.com/mas0061/redmine_plugin_display_role'
   author_url 'https://github.com/mas0061'
 
